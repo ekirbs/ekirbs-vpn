5449aaf66b320ff79e5e37679078e1278ff250d9
Eric K <erickirberger@gmail.com>
2023-06-21 14:11:30 -0400

    working on server structure and api use

--- proxy-server.js
+++ proxy-server.js
@@ -4,6 +4,13 @@ const httpProxy = require('http-proxy');
 const app = express();
 const proxy = httpProxy.createProxyServer();
 
+proxy.on('error', (err, req, res) => {
+  console.error('Proxy server error:', err);
+  res.status(500).send('Proxy server error');
+});
+
+app.use(express.static('public'));
+
 app.all('*', (req, res) => {
   // Forward the request to the target server
   proxy.web(req, res, { target: 'http://example.com' });
